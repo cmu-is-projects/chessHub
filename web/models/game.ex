d02659f6e7f0054d8af2cb9f22783b228a85a255
Aamer <arakla@andrew.cmu.edu>
2016-06-22 22:36:47 -0400

    Upgradged to ecto 2.0/phoenix_ecto 3.0;

--- web/models/game.ex
+++ web/models/game.ex
@@ -22,9 +22,10 @@ defmodule ChessHub.Game do
   If no params are provided, an invalid changeset is returned
   with no validation performed.
   """
-  def changeset(model, params \\ :empty) do
+  def changeset(model, params \\ %{}) do
     model
     |> cast(params, @required_fields, @optional_fields)
+    |> validate_required([:white, :black, :event, :site, :date, :round, :moves])
   end
 
   defimpl Poison.Encoder, for: ChessHub.Game do
